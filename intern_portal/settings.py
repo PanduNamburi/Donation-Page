e87c96bcd7b9e13500dcee0127741f90e1b91f7d
PANDU NAMBURI <pandunamburi@PANDUs-MacBook-Air.local>
2025-08-03 12:51:33 +0530

    Add deployment configuration for Render

--- intern_portal/settings.py
+++ intern_portal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j9w(@p!s17wyo6oq5=0te^+=lr23ra*)#d*h1#vlmkkeboz6l='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'donation-page-oseg.onrender.com', '.onrender.com']
 
 
 # Application definition
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
